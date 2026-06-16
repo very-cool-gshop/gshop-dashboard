867076ea5c2230f729dd896ea277c0117cc8553e
gary <yellow90249@gmail.com>
2026-06-16 10:59:30 +0800

    feat: update

--- app/app.vue
+++ app/app.vue
@@ -25,9 +25,9 @@
     }
   })
 
-  const title = 'Nuxt Dashboard Template'
+  const title = 'GShop Dashboard'
   const description =
-    'A professional dashboard template built with Nuxt UI, featuring multiple pages, data visualization, and comprehensive management capabilities for creating powerful admin interfaces.'
+    'A professional dashboard built with Nuxt UI'
 
   useSeoMeta({
     title,
--- app/components/TeamsMenu.vue
+++ app/components/TeamsMenu.vue
@@ -1,78 +1,27 @@
 <script setup lang="ts">
-  import type { DropdownMenuItem } from '@nuxt/ui'
-
   defineProps<{
     collapsed?: boolean
   }>()
 
-  const teams = ref([
-    {
-      label: 'Nuxt',
-      avatar: {
-        src: 'https://github.com/nuxt.png',
-        alt: 'Nuxt'
-      }
-    },
-    {
-      label: 'NuxtHub',
-      avatar: {
-        src: 'https://github.com/nuxt-hub.png',
-        alt: 'NuxtHub'
-      }
-    },
-    {
-      label: 'NuxtLabs',
-      avatar: {
-        src: 'https://github.com/nuxtlabs.png',
-        alt: 'NuxtLabs'
-      }
+  const team = {
+    label: 'GShop',
+    avatar: {
+      src: 'https://github.com/nuxt.png',
+      alt: 'GShop'
     }
-  ])
-  const selectedTeam = ref(teams.value[0])
-
-  const items = computed<DropdownMenuItem[][]>(() => {
-    return [
-      teams.value.map((team) => ({
-        ...team,
-        onSelect() {
-          selectedTeam.value = team
-        }
-      })),
-      [
-        {
-          label: '建立團隊',
-          icon: 'i-lucide-circle-plus'
-        },
-        {
-          label: '管理團隊',
-          icon: 'i-lucide-cog'
-        }
-      ]
-    ]
-  })
+  }
 </script>
 
 <template>
-  <UDropdownMenu
-    :items="items"
-    :content="{ align: 'center', collisionPadding: 12 }"
-    :ui="{ content: collapsed ? 'w-40' : 'w-(--reka-dropdown-menu-trigger-width)' }"
-  >
-    <UButton
-      v-bind="{
-        ...selectedTeam,
-        label: collapsed ? undefined : selectedTeam?.label,
-        trailingIcon: collapsed ? undefined : 'i-lucide-chevrons-up-down'
-      }"
-      color="neutral"
-      variant="ghost"
-      block
-      :square="collapsed"
-      class="data-[state=open]:bg-elevated"
-      :class="[!collapsed && 'py-2']"
-      :ui="{
-        trailingIcon: 'text-dimmed'
-      }"
-    />
-  </UDropdownMenu>
+  <UButton
+    v-bind="{
+      ...team,
+      label: collapsed ? undefined : team.label
+    }"
+    color="neutral"
+    variant="ghost"
+    block
+    :square="collapsed"
+    :class="[!collapsed && 'py-2 justify-start hover:bg-transparent cursor-default']"
+  />
 </template>
